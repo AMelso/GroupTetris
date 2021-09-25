6ad49df4d7615af455fd522531c7dd6286463a60
Jennserene <Jennserene@gmail.com>
2021-09-25 16:25:53 -0400

    Attached leaderboard frontend to firebase backend

--- src/components/Pages/LeaderBoard.js
+++ src/components/Pages/LeaderBoard.js
@@ -1,9 +1,11 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import SidebarNav from './SidebarNav'
 import { Icon } from 'semantic-ui-react'
 import HighestScore from './GameScore/HighestScore'
 import TotalScore from './GameScore/TotalScore'
 import MyScore from './GameScore/MyScore'
+import { GetHighestScores, GetMyScores, GetTotalScores } from './GameScore/GetFirebaseScores'
+
 const LeaderBoard = () => {
   //set up score manually for now in const default Score List
   //refers to "highest game score tab in the code"
@@ -15,14 +17,27 @@ const LeaderBoard = () => {
   {score:10,name:"Krish"}
 ]
 //when we will integrate firebase Api we ill use setGameScoreList function to propagate API
-  const[highestScoreList] = useState(defaultScores);
+  const[ highestScoreList, setHighestScoreList ] = useState([]);
   // refers to 'My highest games tab in the code"
-  const[myScoreList] =useState([{date:'2021-09-09 11:02:22',score:130},{date:'2021-09-10 01:02:22',score:120},{date:'2021-09-011 12:02:22',score:50}]);
+  const[ myScoreList, setMyScoreList ] =useState([]);
   //refers to "Total score tab in the code"
-  const[totalScoreList] =  useState([{name:'Mike',score:50},{name:'Herry',score:80},{name:'Debra',score:100},{name:'David',score:90}]);
+  const[ totalScoreList, setTotalScoreList ] =  useState([]);
   // change step for tab related to className={activeStep}
   const[activeStep,setStep] = useState(0);
   //we will connect firebase api here using useEffect and save api data response in setGameScoreList
+
+  useEffect(() => {
+    const RetrieveScores = async () => {
+      const TotalScores = await GetTotalScores()
+      setTotalScoreList(TotalScores)
+      const HighestScores = await GetHighestScores()
+      setHighestScoreList(HighestScores)
+      const MyScores = await GetMyScores()
+      setMyScoreList(MyScores)
+    }
+    RetrieveScores()    
+  }, [])
+
     return (
 
         <div className="row">
@@ -44,7 +59,7 @@ const LeaderBoard = () => {
 				</nav>
 				<div className="tab-content py-3 px-3 px-sm-0" id="nav-tabContent">
 					<div className={activeStep === 0 ? "tab-pane fade show active" : "tab-pane fade"}>
-                <TotalScore gameScoreList={totalScoreList}/>
+            <TotalScore gameScoreList={totalScoreList}/>
 					</div>
 					<div className={activeStep === 1 ? "tab-pane fade show active" : "tab-pane fade"}>
             <HighestScore gameScoreList={highestScoreList}/>

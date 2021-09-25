6dd688664e3e6453499031de429aa01fc26a4ae1
Jeffery Legg <jeffrey.d.legg@gmail.com>
2021-09-25 13:38:38 -0400

    AI Updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import { useAuthState } from '../firebase'
+import TetrisDemo from './tetrisDemo/Tetris'
 
 export const Home = () => {
   const { user } = useAuthState() // Returns user object, can access user.UID from that.
@@ -6,6 +7,7 @@ export const Home = () => {
   return (
     <>
       <h1>Welcome {user?.email}</h1>
+      <TetrisDemo />
     </>
   )
 }

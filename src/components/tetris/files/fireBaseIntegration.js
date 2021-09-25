6a0bb2f7520ce1b07cc9e9a66eb36b7dcf920bc2
Jennserene <Jennserene@gmail.com>
2021-09-25 15:03:15 -0400

    Commented out

--- src/components/tetris/files/fireBaseIntegration.js
+++ src/components/tetris/files/fireBaseIntegration.js
@@ -6,11 +6,11 @@ const auth = getAuth()
 let userUID = ''
 let userName = ''
 onAuthStateChanged(auth, (user => {
-  userUID = user.uid
-  userName = user.displayName
+  userUID = user.uid // Get user's UID
+  userName = user.displayName // Get user's display name
 }))
 
-
+// Update the total points in the userUID document in the users collection
 export const UpdatePoints = async (score) => {
   const updateRef = doc(db, "users", userUID)
   await updateDoc(updateRef, {
@@ -18,37 +18,39 @@ export const UpdatePoints = async (score) => {
   })
 }
 
+// Get the total points in the userUID document in the users collection
 export const GetPoints = async () => {
   const docRef = doc(db, "users", userUID)
-  const docSnap = await getDoc(docRef)
-  const points = parseInt(docSnap.data().points)
+  const docSnap = await getDoc(docRef) // Get the information out of the userUID document
+  const points = parseInt(docSnap.data().points) // Specifically get the points value
   // console.log('RETRIEVED POINTS: ', points)
   return points
 }
 
 export const UpdateLeaderBoards = async (total, score) => {
 
-  let displayName = userName
-  if (userName === null) {
+  // Does a username exist? 
+  let displayName = userName // If so set displayName to that username
+  if (userName === null) { // If not, set displayName to Anonymous
     displayName = 'Anonymous'
   }
 
   // Set the total points leaderboard entry
-  await setDoc(doc(db, 'leaderboards/default/TotalPoints', userUID), {
+  await setDoc(doc(db, 'leaderboards/default/TotalPoints', userUID), { // Set the userUID document in the TotalPoints leaderboard to the values below
     name: displayName,
     score: total,
     timestamp: serverTimestamp()
   })
 
   // Set the Individual Game points leaderboard entry IF it is >= current highscore
-  const docRef = doc(db, "leaderboards/default/IndividualGamePoints", userUID)
-  const docSnap = await getDoc(docRef)
+  const docRef = doc(db, "leaderboards/default/IndividualGamePoints", userUID) // path to read
+  const docSnap = await getDoc(docRef) // Get data from document userUID
   let highscore = 0
-  if (docSnap.data().score) {
-    highscore = parseInt(docSnap.data().score)
+  if (docSnap.data().score) { // Does a highscore already exist in docSnap?
+    highscore = parseInt(docSnap.data().score) // Set highscore to that score
   }
-  if (score >= highscore) {
-    await setDoc(doc(db, 'leaderboards/default/IndividualGamePoints', userUID), {
+  if (score >= highscore) { // Is the current game's score higher than the pre-existing high score?
+    await setDoc(doc(db, 'leaderboards/default/IndividualGamePoints', userUID), { // Set the userUID document in the IndividualGamePoints leaderboard to the values below
       name: displayName,
       score: score,
       timestamp: serverTimestamp()
@@ -56,7 +58,7 @@ export const UpdateLeaderBoards = async (total, score) => {
   }
 
   // Set personal Individual Game leaderboard
-  await addDoc(collection(db, `users/${userUID}/personalLeaderboard`), {
+  await addDoc(collection(db, `users/${userUID}/personalLeaderboard`), { // path to write values below
     score: score,
     timestamp: serverTimestamp()
   })

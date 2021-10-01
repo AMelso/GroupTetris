fb90aea978d9c0b023a26621da0da4aed5299dc5
amelso <anthonymelsoner@gmail.com>
2021-09-30 23:24:58 -0400

    refactored logic, conditional rendering of upgrade button

--- src/components/Pages/UpgradeFiles/DropSpeed.js
+++ src/components/Pages/UpgradeFiles/DropSpeed.js
@@ -6,6 +6,44 @@ import { GetPoints, GetUpgrades, SaveUpgrade, SpendPoints } from './UpgradesFire
 
 
 export const DropSpeedCard = (props) => {
+
+  //UPGRADE COST LOGIC
+  // object of upgrade cost
+  const upgradeToLevelCost = {
+    "1":500,
+    "2":1000,
+    "3":2000,
+    "4":3000,
+    "5":4000
+  }
+
+  // assign to a variable, which adds 1 to the current level from props
+  // if current level is 5, set upgradeToLevel as 6, over max, cant do, handled below
+  if (props.dropSpeedLevel >= 5) {
+    var upgradeButton = 
+      "Max Level"
+  } else {
+    var upgradeToLevel = (1 + props.dropSpeedLevel)
+    // set UpgradeCost to upgrade level cost in upgradeToLevel object of level:cost
+    // then render upgrade button.
+    var UpgradeCost = upgradeToLevelCost[upgradeToLevel]
+    var upgradeButton = 
+    <div className='button'>
+      <Button as='div' labelPosition='left'>
+        <Label basic pointing='right'>
+          {/* // pass in next level */}
+          Upgrade: {upgradeToLevel}
+        </Label>
+        <Button icon>
+          <Icon name='arrow alternate circle up outline' />
+          {/* // pass in upgrade cost */}
+          {UpgradeCost}
+        </Button>
+      </Button>
+    </div>
+  }
+    //////// End upgrade Cost logic
+
   return(
     <Card>
       <Image src="https://www.pikpng.com/pngl/m/576-5768393_transparent-clipart-crystal-ball-png-download.png" />
@@ -19,17 +57,8 @@ export const DropSpeedCard = (props) => {
         </Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <div className='button'>
-          <Button as='div' labelPosition='left'>
-            <Label basic pointing='right'>
-              Upgrade:
-            </Label>
-            <Button icon>
-              <Icon name='arrow alternate circle up outline' />
-              Dynamic Cost
-            </Button>
-          </Button>
-        </div>
+      {/* button conditionally rendered in upgrade cost logic */}
+      {upgradeButton}
       </Card.Content>
     </Card>
   )
--- src/components/Pages/UpgradeFiles/LookAhead.js
+++ src/components/Pages/UpgradeFiles/LookAhead.js
@@ -6,6 +6,45 @@ import { GetPoints, GetUpgrades, SaveUpgrade, SpendPoints } from './UpgradesFire
 
 
 export const LookAheadCard = (props) => {
+
+
+  //UPGRADE COST LOGIC
+  // object of upgrade cost
+  const upgradeToLevelCost = {
+    "1":500,
+    "2":1000,
+    "3":2000,
+    "4":3000,
+    "5":4000
+  }
+
+  // assign to a variable, which adds 1 to the current level from props
+  // if current level is 5, set upgradeToLevel as 6, over max, cant do, handled below
+  if (props.lookAheadLevel >= 5) {
+    var upgradeButton = 
+      "Max Level"
+  } else {
+    var upgradeToLevel = (1 + props.lookAheadLevel)
+    // set UpgradeCost to upgrade level cost in upgradeToLevel object of level:cost
+    // then render upgrade button.
+    var UpgradeCost = upgradeToLevelCost[upgradeToLevel]
+    var upgradeButton = 
+    <div className='button'>
+      <Button as='div' labelPosition='left'>
+        <Label basic pointing='right'>
+          {/* // pass in next level */}
+          Upgrade: {upgradeToLevel}
+        </Label>
+        <Button icon>
+          <Icon name='arrow alternate circle up outline' />
+          {/* // pass in upgrade cost */}
+          {UpgradeCost}
+        </Button>
+      </Button>
+    </div>
+  }
+    //////// End upgrade Cost logic
+
   return(
     <Card>
       <Image src="https://www.pikpng.com/pngl/m/576-5768393_transparent-clipart-crystal-ball-png-download.png" />
@@ -20,17 +59,8 @@ export const LookAheadCard = (props) => {
         </Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <div className='button'>
-          <Button as='div' labelPosition='left'>
-            <Label basic pointing='right'>
-              Upgrade:
-            </Label>
-            <Button icon>
-              <Icon name='arrow alternate circle up outline' />
-              Dynamic Cost
-            </Button>
-          </Button>
-        </div>
+      {/* button conditionally rendered in upgrade cost logic */}
+      {upgradeButton}
       </Card.Content>
     </Card>
   )

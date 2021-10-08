48e29ee310de9c99d5c1868f0f13c3a8985d491d
amelso <anthonymelsoner@gmail.com>
2021-10-07 20:00:08 -0400

    demo style change

--- src/components/tetris/tetrisDemo/styles/StyledStage.js
+++ src/components/tetris/tetrisDemo/styles/StyledStage.js
@@ -8,8 +8,8 @@ export const StyledStage = styled.div`
   );
   grid-template-columns: repeat(${props => props.width}, 1fr);
   grid-gap: 1px;
-  border: 2px solid #333;
+  border: 2px solid #ddd;
   width: 100%;
   max-width: 25vw;
-  background: #111;
+  background: #f2f2f2;
 `
